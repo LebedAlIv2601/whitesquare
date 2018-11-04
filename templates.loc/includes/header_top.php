a12a0b5e54fa99509e592fd254417dabe19d2814
LebedAlIv2601 <32431414+LebedAlIv2601@users.noreply.github.com>
2018-11-04 20:27:16 +0300

    Add files via upload

---
+++ templates.loc/includes/header_top.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+
+	<title>Template</title>
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	
+	<!-- favicon -->
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+
+	<!-- fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:300,300i,400&amp;subset=cyrillic,latin-ext" rel="stylesheet"> 
+	<!-- libs -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<!-- main -->
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+			  <div class="btn-group mr-2" role="group" aria-label="First group">
+    <button type="button" class="btn1">HOME</button>
+    <button type="button" class="btn1">ABOUT US</button>
+    <button type="button" class="btn1">SERVICES</button>
+    <button type="button" class="btn1">PARTNERS</button>
+    <button type="button" class="btn1">CUSTOMERS</button>
+    <button type="button" class="btn1">PROJECTS</button>
+    <button type="button" class="btn1">CAREERS</button>
+    <button type="button" class="btn1">CONTACT</button>
+  </div>
+  </div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
